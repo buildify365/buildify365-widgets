a89f948bedbe4dcbefa7c154de08d75d719142da
buildify365 <buildify365@gmail.com>
2026-02-28 21:59:58 +0900

    Add files via upload

---
+++ series/minecraft-buildup.js
@@ -0,0 +1,25 @@
+BuildifyTOC.render('minecraft-buildup', {
+  title: '마크빌드업 시리즈',
+  subtitle: '마인크래프트 서버 구축부터 플러그인 개발까지',
+  icon: '📋',
+  accent: '#1a1a1a',
+  bg: '#f9fafb',
+  highlightCurrent: true,
+  collapsible: true,
+  defaultOpen: true,
+  animation: true,
+  showPrevNext: true,
+  pnStyle: 'full',
+  listMode: 'all',
+  maxVisible: 5,
+  linkTarget: '_blank',
+  items: [
+    { num:'00', title:'마인크래프트 서버 만들기 + 플러그인 개발 로드맵 (2026 최신) | 마크빌드업 EP.00', sub:'2026 최신 · 시리즈 소개', url:'https://buildify365.tistory.com/80' },
+    { num:'01', title:'내 컴퓨터로 마인크래프트 서버 열기 (자바 에디션, 윈도우) | 마크빌드업 EP.01', sub:'자바 에디션 · 윈도우', url:'https://buildify365.tistory.com/81' },
+    { num:'02', title:'24시간 마인크래프트 서버 무료로 열기 – GCP 클라우드 활용 | 마크빌드업 EP.02', sub:'GCP 클라우드 활용', url:'https://buildify365.tistory.com/82' },
+    { num:'03', title:'마인크래프트 서버 도메인 연결 방법 (고정 IP + 무료 도메인) | 마크빌드업 EP.03', sub:'고정 IP + 무료 도메인', url:'https://buildify365.tistory.com/83' },
+    { num:'04', title:'마인크래프트 Paper 서버 구축 + 플러그인 설치 방법 (2026) | 마크빌드업 EP.04', sub:'2026 · Paper 서버', url:'https://buildify365.tistory.com/84' },
+    { num:'05', title:'마인크래프트 플러그인 만들기 – 환경 세팅부터 배포까지 (IntelliJ + Paper API) | 마크빌드업 EP.05', sub:'IntelliJ + Paper API', url:'https://buildify365.tistory.com/85' },
+    { num:'06', title:'Paper API 핵심 개념 + 미니게임 플러그인 실습 | 마크빌드업 EP.06', sub:'게임개발', url:'https://buildify365.tistory.com/89' }
+  ]
+});
