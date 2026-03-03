57b7049d0d778ef1f42e827de48bc1da72ea3afa
buildify365 <buildify365@gmail.com>
2026-03-03 13:47:33 +0900

    Add files via upload

---
+++ series/zomboid-series.js
@@ -0,0 +1,21 @@
+BuildifyTOC.render('zomboid-series', {
+  title: '좀보이드 서버 시리즈',
+  subtitle: '좀보이드 서버 설치 부터 설정까지',
+  icon: '🔥',
+  accent: '#1a1a1a',
+  bg: '#f9fafb',
+  highlightCurrent: true,
+  collapsible: true,
+  defaultOpen: true,
+  animation: true,
+  showPrevNext: true,
+  pnStyle: 'full',
+  listMode: 'all',
+  maxVisible: 5,
+  linkTarget: '_blank',
+  items: [
+    { num:'01', title:'[무료] 좀보이드 멀티 24시간 서버 여는 법 (GCP + 우분투)', sub:'', url:'https://buildify365.tistory.com/1' },
+    { num:'02', title:'좀보이드 멀티 서버 모드 추가 방법 (데디케이트 서버 모드 설정 가이드)', sub:'', url:'https://buildify365.tistory.com/79' },
+    { num:'03', title:'추가 에피소드 (추후 수요에 따라 편성 예정)', sub:'', url:'', comingSoon:true }
+  ]
+});
